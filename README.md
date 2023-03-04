cd7fb7b249232a1cfc07e38985fbe172f41c0cf2
Seven <edrezner7@gmail.com>
2023-03-03 20:32:57 -0500

    Added a README file and comments on the JavaScript

---
+++ README.md
@@ -0,0 +1,26 @@
+# Work Day Scheduler
+
+## Description
+This application is a day scheduler for work hours from nine to five. The current day is displayed at the top of the screen. Tasks to be completed can be placed in each time block, and they are color coded. A row colored gray represents a time block in the past, a red colored row is the current hour and a green colored row represents a time block in the future.
+
+A save button next to each time block will store the info entered in the accompanying text field so that the task is not lost if the page is closed or navigated away from.
+
+[GitHub Repo](https://github.com/edrezner/Work-Day-Scheduler) 
+
+[Website](https://edrezner.github.io/Work-Day-Scheduler/)
+
+Preview Image:
+
+![Work Day Scheduler](./assets/images/preview.png)
+
+## Usage
+Enter tasks, events, meetings, etc. in the input fields for that appropriate hour. Press the save button to the right of the input field to save the text. Rows are color coded by time, gray is past, red is present hour, and green is future. 
+
+## Credits
+1. Natasha Mann (tutor) - Helped me complete the code in the JavaScript file from lines 37-56 and lines 65-69.
+
+2. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/Number This page introduced me on how to convert a string to a number using the Number() constructor.
+
+3. https://api.jquery.com/category/traversing/ This page used as guidance on how to target elements and traverse the DOM using jQuery.
+
+4. https://stackoverflow.com/questions/1206739/find-all-elements-on-a-page-whose-element-id-contains-a-certain-text-using-jquer This post helped me to start writing the code beginning on lines 38 and 65 by targetting the elements that contained specified text.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+//Code is wrapped using $(dociment).ready() to ensure the code runs on full load/render.
 $(document).ready(function workScheduler () {
   var dayJs = dayjs();
   // TODO: Add a listener for click events on the save button. This code should
@@ -10,12 +12,16 @@ $(document).ready(function workScheduler () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
 
+  /* This function saves the input from the text field; 'this' is used in reference to the 
+  save button. The key in local storage is set to match the div id, and the value is the string
+  that occupies the text input field with class 'description'. These elements are selected using
+  the parent() and siblings() methods. */ 
   var saveBtn = $('.saveBtn');
   saveBtn.click(function () {
     var hourId = $(this).parent().attr('id');
     var scheduleEntryData = $(this).siblings('.description').val();    
     localStorage.setItem(hourId, scheduleEntryData);
-  })
+  });
   
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -23,9 +29,13 @@ $(document).ready(function workScheduler () {
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
 
+  /* This function dynamically updates the classes to past, present, or future depending on the hour associated
+  with the div element. This is done by converting the 24 hour format from Day.js into a number, taking the div id
+  'hour-x' and using the split method at the hyphen to create two arrays with the number (currently a string) occupying the second index.
+  That string is also converted to a number using the Number constructor so it can be compared against the current Day.js hour using conditional
+  statements.*/ 
   var hourDay = Number(dayJs.format('H'));
-  $('div[id*="hour-"]').each(function applyClasses(index) {
-    console.log(this);
+  $('div[id*="hour-"]').each(function applyClasses() {
     var id = $(this).attr('id');
     var hour = Number(id.split('-').pop());
     
@@ -50,19 +60,17 @@ $(document).ready(function workScheduler () {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
 
+  // Grabs the current values in local storage and applies them to their corresponding text box matched by div id.
   $('div[id*="hour-"]').each(function applyLocalStorage() {
     var id = $(this).attr('id');
     var scheduleText = localStorage.getItem(id);
-    if (scheduleText !== null) {
-      $(this).find('.description').val(scheduleText);
-    }
-    console.log(scheduleText);
-    console.log(id);
-  })
+    $(this).children('.description').val(scheduleText);
+  });
 
   // TODO: Add code to display the current date in the header of the page.
+
+  // This uses Day.js day/month/numbered day to apply the current day to the header.
   var day = dayJs.format('dddd, MMMM D');
   $('#currentDay').text(day);
 });
